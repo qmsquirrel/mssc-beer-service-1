3e6d1db130c82802bcaf9e068ab4846e98485472
Ralph L <lettau.ralph@gmail.com>
2022-03-08 11:18:45 +0100

    Beer Data Model

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerPageList.java
@@ -0,0 +1,16 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BeerPageList extends PageImpl<BeerDTO> {
+    public BeerPageList(List<BeerDTO> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPageList(List<BeerDTO> content) {
+        super(content);
+    }
+}
